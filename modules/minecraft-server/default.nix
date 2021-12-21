2a315c63c4c0909b50acf1f4701d84522ff5203c
Kevin Cruse <kevincruse7@gmail.com>
2021-12-20 19:18:10 -0800

    Finalized vanilla Minecraft server setup

--- modules/minecraft-server/default.nix
+++ modules/minecraft-server/default.nix
@@ -6,30 +6,63 @@
 in {
     environment.systemPackages = [ minecraft-server ];
 
+    # Allow traffic for Dynmap and Minecraft servers
     networking.firewall = {
-        allowedTCPPorts = [ 25565 ];
-        allowedUDPPorts = [ 25565 ];
+        allowedTCPPorts = [
+            8123
+            25565
+        ];
+
+        allowedUDPPorts = [
+            8123
+            25565
+        ];
     };
 
-    systemd = {
-        services.minecraftd = {
-            description = "Minecraft server daemon";
-            enable = true;
-            wantedBy = [ "multi-user.target" ];
+    # MySQL database for LogBlock plugin
+    services.mysql = {
+        enable = true;
+        package = pkgs.mariadb;
+        bind = "127.0.0.1";  # Only allow connections from localhost
 
-            after = [
-                "local-fs.target"
-                "network.target"
-                "multi-user.target"
-            ];
+        # MySQL settings required by LogBlock
+        settings.mysqld = {
+            default_storage_engine = "INNODB";
+            innodb_buffer_pool_size = "256M";
+            key_buffer_size = "128M";
+            max_connections = 100;
+            query_cache_size = 0;
+            skip_name_resolve = true;
+        };
 
-            serviceConfig = {
-                Group = "minecraft";
-                ExecStart = "${minecraft-server}/bin/minecraftd start";
-                ExecStop = "${minecraft-server}/bin/minecraftd stop";
-                Type = "forking";
-                User = "minecraft";
-            };
+        initialDatabases = [{
+            name = "logblock";
+            schema = ./logblock-db-schema.sql;
+        }];
+
+        initialScript = pkgs.writeText "logblock-db-users.sql" ''
+            CREATE USER minecraft@127.0.0.1;
+            GRANT ALL PRIVILEGES ON logblock.* TO minecraft@127.0.0.1;
+        '';
+    };
+
+    systemd.services.minecraftd = {
+        enable = false;
+        description = "Minecraft server daemon";
+        wantedBy = [ "multi-user.target" ];
+
+        after = [
+            "local-fs.target"
+            "network.target"
+            "multi-user.target"
+        ];
+
+        serviceConfig = {
+            Type = "forking";
+            User = "minecraft";
+            Group = "minecraft";
+            ExecStart = "${minecraft-server}/bin/minecraftd start";
+            ExecStop = "${minecraft-server}/bin/minecraftd stop";
         };
     };
 
@@ -37,11 +70,11 @@ in {
         groups.minecraft = {};
 
         users.minecraft = {
-            createHome = true;
+            isSystemUser = true;
             description = "Minecraft server user";
             group = "minecraft";
             home = "/srv/minecraft";
-            isSystemUser = true;
+            createHome = true;
             shell = pkgs.bash;
         };
     };
